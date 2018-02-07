d40f2f739dcab0c64fe341720983f4f04d3f7705
svrrealism <svrrealists@gmail.com>
2018-02-07 09:48:44 -0500

    Add recap points.

--- big_o.rb
+++ big_o.rb
@@ -19,5 +19,18 @@ puts """
     'My worst can be explained as: I will decrease the amount
     of steps it takes for me to complete by half, per n I recieve'
 
+RECAP:
+  • The 'O' stands for Operations
+
+  •  Binary search is a lot faster than simple search.
+
+  • O(log n) is faster than O(n), but it gets a lot faster once the list of
+    items you’re searching through grows.
+
+  • Algorithm speed isn’t measured in seconds.
+
+  • Algorithm times are measured in terms of growth of an algorithm.
+
+  • Algorithm times are written in Big O notation.
 
 """
