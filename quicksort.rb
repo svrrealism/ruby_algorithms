61ec8d7fde89d7eddb6f26833832720e2511aa90
svrrealism <svrrealists@gmail.com>
2018-02-21 09:16:57 -0500

    Update Quicksort notes with diagram

--- quicksort.rb
+++ quicksort.rb
@@ -8,9 +8,37 @@ New skill to apply to problems; a recursive technique called:
 
 
 
+First thing we should asked when 'stumped' is...
+
+---> 'can I use divide and conquer?'
+
+Quicksort is much faster than selection sort
 
 
+Three examples of Divide and Conquer:
 
+1). Suppose you are a farmer with some land.
+
+
+PROBLEM: You want to:
+        1). Divide this land into even, square plots,
+        2). They should be as big as possible.
+
+TRANSLATED QUESTION: What is the maximum of amount of even square plots of the
+                     largest size that can be created from an input of an originating
+                     land plot size of: 1680 width * 640 height ?
+
+PROBLEM VISUALLY:
+
+
+Land plot given:
+
+        1680 meters
+***************************
+***************************
+*************************** 640 meters
+***************************
+***************************
 
 
 
