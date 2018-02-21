11dd7b77b5db83a679efdacbfa9ea665f4fc28d7
svrrealism <svrrealists@gmail.com>
2018-02-20 21:41:12 -0500

    Start odyssey practice file, complete recursion.

---
+++ odyssey.rb
@@ -0,0 +1,166 @@
+#write binary search 3 times
+
+
+def binary_search(sorted_array, value)
+  low = sorted_array[0]
+  high = sorted_array.length - 1
+
+  while low <= high
+
+    mid = (low + high)/2
+    guess = sorted_array[mid]
+
+    if guess == value
+      return mid
+    end
+
+    if guess > value
+      high = mid - 1
+    else
+      low = mid + 1
+    end
+  end
+
+  return nil
+end
+
+
+def b_search(sorted_array, value)
+  left = sorted_array[0]
+  right = sorted_array.length - 1
+
+  while left <= right
+    mid = (right+left)/2
+    guess = sorted_array[mid]
+
+    if guess == value
+      return mid
+    end
+
+    if guess < value
+      left = mid + 1
+    else
+      right = mid - 1
+    end
+  end
+
+  return nil
+end
+
+
+
+def bin_search(sorted_collection, value)
+  low = sorted_collection[0]
+  high = sorted_collection.length - 1
+
+  while low <= high
+    mid = (low + high)/2
+
+    if sorted_collection[mid] == value
+      return mid
+    elsif sorted_collection[mid] < value
+      low = mid + 1
+    else
+      high = mid - 1
+    end
+  end
+
+  return nil
+end
+
+gigs = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
+
+p binary_search(gigs, 256)
+p b_search(gigs, 512)
+p bin_search(gigs, 128)
+
+
+##########################################
+
+#Write Selection Sort Algorithm 3 times.
+
+
+def smallest(arr)
+  tmp = arr[0]
+  tmp_index = 0
+
+  1.upto(arr.size-1) do |i|
+    if arr[i] < tmp
+      tmp = arr[i]
+      tmp_index = i
+    end
+  end
+  return tmp_index
+end
+
+def selection_sort(arr)
+  new_arr = []
+
+  1.upto(arr.size) do |i|
+    smallest_number = smallest(arr)
+    new_arr.push(arr.delete_at(smallest_number))
+  end
+  return new_arr
+end
+
+def find_small_int(arr)
+  tmp = arr[0]
+  tmp_index = 0
+
+  1.upto(arr.size-1) do |i|
+    if arr[i] < tmp
+      tmp = arr[i]
+      tmp_index = i
+    end
+  end
+  return tmp_index
+end
+
+
+def selecty_sort(arr)
+  new_arr = []
+
+  (arr.size).times do |i|
+    smallest_int = find_small_int(arr)
+    new_arr.push(arr.delete_at(smallest_int))
+  end
+
+  return new_arr
+end
+
+
+def get_smallest(arr)
+  tmp = arr[0]
+  tmp_index = 0
+
+  1.upto(arr.size-1) do |i|
+    if arr[i] < tmp
+      tmp = arr[i]
+      tmp_index = i
+    end
+  end
+  return tmp_index
+end
+
+def selecty_sorty(arr)
+  new_arr = []
+
+  n = arr.size
+  n.times do |i|
+    smallest_integer = get_smallest(arr)
+    new_arr.push(arr.delete_at(smallest_integer))
+  end
+  return new_arr
+end
+
+blah = gigs.shuffle()
+blah2 = gigs.shuffle()
+blah3 = gigs.shuffle()
+
+p blah
+p blah2
+p blah3
+
+p selection_sort(blah)
+p selecty_sort(blah2)
+p selecty_sorty(blah3)
--- recursion.rb
+++ recursion.rb
@@ -76,18 +76,260 @@ Method 1 proposes:
 
   Method 2: Proposes recursion
 
+    def find_safe_key(main_box)
+      for i in main_box
+        if i.is_box?
+          find_safe_key(i)
+        elsif i.is_key?
+          return 'key found!'
+        else
+          return nil
+        end
+      end
+    end
+
+
+Loops: may achieve a performance gain for your program
+Recursion: may achieve a performance gain for your programmer.
+
+Always choose within the right context.
+
+
+In every recursive program there is:
+  - Base case
+  - Recursive case
+
+Consider the following infinite loop:
+
+def infinite(n)
+  print n
+  infinite(n-1)
+end
+
+The above prints the result of n-1 recursively, infinitely. There is no point in which the recursive case will end.
+
+To correct this:
+
+def no_longer_infinite(n)
+
+  print n
+  if n <= 0
+    return n
+  else
+    no_longer_infinite(n-1)
+  end
+end
+
+Recursion is used when it makes the solution clearer.
+
+
+---------------------
+Introducing the Stack
+---------------------
+Our computers use a stack called the 'call stack'
+
+The CALL STACK is important to programming and recursion.
+
+Suppose you host a party and you keep a todo list in the form of a stack of index cards.
+
+Recall that in arrays we can insert and delete randomly.
+
+In Stacks
+  - Insertions = only get put onto the top of the stack (PUSH)
+  - Reading = will always be the topmost item AND it's taken off of the list to read (POP)
+
+For example, a stack could be to get:
+
+Burgers  <---- Top of the stack.
+Buns
+Plates
+Beer
+More Beer
+
+implementation in code could look similar to:
+
+get_party_stuff = ['More Beer', 'Beer', 'Plates', 'Buns', 'Burgers']
+
+
+This data structure is called a stack.
+
+Have been using a stack the whole time.
+
+
+HOW CALL STACK WORKS
+--------------------
+
+Let's analyze the above function again albeit slightly modified:
+
+    def no_longer_infinite(n)
+      num = n
+      print num
+
+      if num <= 0
+        return num
+      else
+        no_longer_infinite(num-1)
+      end
+    end
+
+
+1). Say we call no_longer_infinite(2) = 2
+2). num gets set to 2, which is allocated in memory
+3). But then no_longer_infinite(2-1) = 1 is called which gets put on top of the stack in memory
+
+When you call a function with a function, the main function is paused in a partially completed state.
+
+So the stack looks like:
+
+1 <- Top of the stack (recursive case)
+2 <- Base case
+
+implemented in pseudocode:
+
+[2,1]
+
+
+
+--------------------------------------------------------------------------------------------------------
+IMPORTANT:
+*** We've effectively saved variables from the calls of different functions. This is the call stack. ***
+---------------------------------------------------------------------------------------------------------
+
+
+Exercise 3.1
+
+Suppose there exists a stack:
+
+-------------
+hello_world2
+name: 'alice'
+-------------
+hello_world
+name: 'alice'
+-------------
+
+Q: What information can be given based on the above?
+
+A: Rationalization:
+    - The hello_world function is the main function returning a var into a call stack in memory
+
+    - The hello_world2 function executed and placed a var of name into call stack in memory but
+      at the top of the stack
+
+    - the calling of these functions create a stack in which the total of variable values of
+      differing scopes is populated with.
 
 
 
 
 
+-------------------------
+CALL STACK WITH RECURSION
+-------------------------
+
+Take factorial for instance...
+
+For any number n, it's factorial, n! = n * n-1 * n-2 * ... * 2 * 1
+
+def factorial(n)
+  if n == 1
+    return 1
+  else
+    return n * factorial(n-1)
+  end
+end
+
+Say we call factorial(3)
+
+Line by line:
+
+  - The top most box in the stack tells us what call to factorial we are currently on.
+
+  CODE                          CALL STACK
+
+factorial(3)                    factorial(3) | n = 3  <--- First call to factorial, n = 3
+
+if n == 1                       factorial(3) | n = 3
+
+return 1                        factorial(3) | n = 1; program exits, returns the value 1 if this condition is true.
+
+else                            factorial(3) | n = 3; still in the base case of the factorial function.
+
+return n * factorial(n-1)       factorial(2) | n = 2; recursive call (recursive case)
+                                factorial(3) | n = 3; base case
+
+                                  NOTE: You cannot access factorial(2)'s n-var from the base case or any other case.
 
 
+------------------------------- Now in factorial(2) call ----------------------------------------
+
+if n == 1                       factorial(2) | n = 2
+
+return 1                        factorial(2) | n = 1; program exits, returns a value of 1 if this condition is true.
+
+else                            factorial(2) | n = 2
+
+return n * factorial(n-1)       factorial(1) | n = 1; recursive call (recursive case)
+                                factorial(2) | n = 2; recursive call (recursive case)
+                                factorial(3) | n = 3; base case
+
+                                NOTE: Again you cannot access any of the factorial calls' n-vars through each other.
+                                      ex). factorial(2)'s as a function has no access to what factorial(1) returns
 
 
+------------------------------- Now in factorial(1) call ----------------------------------------
+
+if n == 1                       factorial(1) | n = 1
+
+return 1                        factorial(1) | n = 1; program will now terminate, returning a value of 1 since n = 1
 
 
+REMEMBER: We INITIATED three 'calls'
+
+BUT...we never COMPLETED an actual call until factorial(1)
+
+STACK
+-----
+factorial(1)  <-- This gets popped off of the stack first, which means it's the first call we return from.
+factorial(2)
+factorial(3)
 
 
+So 1 gets returned FIRST.
+
+
+There's a cost to having a stack with recursion...takes up a lot of memory since you are returning all these values.
+
+If your program's stack grows too big, there are 2 options:
+
+  - Rewrite code to use a loop instead.
+
+  - Use something called TAIL RECURSION...only supported by some languages. Advanced.
+
+
+EXERCISE 3.2
+
+Suppose a recursive function is written that runs forever.
+Computers allocate memory for the base and recursive cases.
+
+Q: What happens to the stack when the function runs forever?
+A: It keeps growing, taking up more address space.
+
+
+RECAP
+
+- Recursion is when a function calls itself.
+
+- Every recursive function has two cases:
+  1. Base case
+  2. Recursive case
+
+- Stack has two operations:
+  1. Push
+  2. Pop
+
+- All functions go onto the call stack
+
+- The call stack can get ginormous, which gobbles up tons and tons of memory
 
 """
