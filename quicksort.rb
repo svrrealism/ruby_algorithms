6c5c09d5591794721014026bfe1e32d6ff786770
svrrealism <svrrealists@gmail.com>
2018-02-21 08:39:59 -0500

    Add beginning of Quicksort, chapter 4

---
+++ quicksort.rb
@@ -0,0 +1,39 @@
+puts """
+
+QUICKSORT - elegant sorting algorithm that's used in practice.
+
+New skill to apply to problems; a recursive technique called:
+
+  DIVIDE AND CONQUER (D&C)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
