8c8d9cb1929a9eee318ce69ab4574928c34e1744
svrrealism <svrrealists@gmail.com>
2018-03-03 16:57:12 -0500

    Add test for PriorityQueue algorithm.

---
+++ priority_queue_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require_relative 'online_priority_queue'
+
+class PriorityQueueTest < Minitest::Test
+
+  def setup
+    @priority_queue = PriorityQueue.new
+  end
+
+  def test_adds_element_in_correct_order
+    @priority_queue << 2
+    @priority_queue << 3
+    @priority_queue << 1
+
+    assert_equal( [nil, 3, 2, 1], @priority_queue.elements )
+  end
+
+  def test_removes_top_element
+    @priority_queue << 2
+    @priority_queue << 3
+    @priority_queue << 1
+
+    assert_equal( 3, @priority_queue.pop )
+    assert_equal( [nil, 2, 1], @priority_queue.elements )
+  end
+end
